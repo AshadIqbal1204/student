024661cd680dff8391128cb66d3137346afbd5da
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-08 23:22:34 +0530

    Arrays with question

---
+++ core java/src/Array/PrintingArraysUsingLoop.java
@@ -0,0 +1,11 @@
+//02) 	wap to create and print array using loop
+package Array;
+
+public class PrintingArraysUsingLoop {
+    public static void main(String[] args) {
+        int arr[]={4,3,6,7};
+        for (int i=0;i<= arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
