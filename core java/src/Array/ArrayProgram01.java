d6e5b4aa13bb890f8bd9894d5b6d566cf2f4ab08
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-24 13:07:04 +0530

    Arrays with question

---
+++ core java/src/Array/ArrayProgram01.java
@@ -0,0 +1,20 @@
+//	01) 	wap to create and print array using without loop
+package Array;
+
+public class ArrayProgram01 {
+    public static void main(String[] args) {
+        int arr[]={2,5,4,7,9,7,4,5,6};
+        System.out.println("Array length");
+        System.out.println(arr.length);
+        System.out.println("Printing Array");
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+        System.out.println(arr[5]);
+        System.out.println(arr[6]);
+        System.out.println(arr[7]);
+        System.out.println(arr[8]);
+    }
+}
