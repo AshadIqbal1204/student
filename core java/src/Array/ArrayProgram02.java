2ccabb15a4fcd9ff1f3794130767cd4454b832ab
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-24 13:07:19 +0530

    Arrays with question

---
+++ core java/src/Array/ArrayProgram02.java
@@ -0,0 +1,11 @@
+//	02) 	wap to create and print array using loop
+package Array;
+
+public class ArrayProgram02 {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6};
+        for (int i=0;i<= arr.length;i++){
+            System.out.println(i);
+        }
+    }
+}
