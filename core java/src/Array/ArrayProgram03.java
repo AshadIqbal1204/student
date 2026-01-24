061ad4b718391456a77ab1348724068ebc2971ce
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-24 13:07:33 +0530

    Arrays with question

---
+++ core java/src/Array/ArrayProgram03.java
@@ -0,0 +1,10 @@
+//	03) 	wap to find length of an array
+package Array;
+
+public class ArrayProgram03 {
+    public static void main(String[] args) {
+        int arr[]={3,5,2,1,7,6,5,8};
+        System.out.println("Array Length");
+        System.out.println(arr.length);
+    }
+}
