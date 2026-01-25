3da54d8bf9c90ba521ad2f2d9f5225601c9cc14e
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-25 13:17:59 +0530

    Arrays with question

---
+++ core java/src/Array/ArrayProgram10.java
@@ -0,0 +1,11 @@
+//	10) 	wap to print array in reverse order
+package Array;
+
+public class ArrayProgram10 {
+    public static void main(String[] args) {
+        int arr[]={3,2,4,5,1,6,7,9,8};
+        for (int i=8;i<= arr.length;i--){
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
