423817c767edccba8d921126fa0aaf4caf8b596c
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-26 13:26:39 +0530

    TypeCasting program with question

---
+++ core java/src/TypeCasting/TypeCasting13.java
@@ -0,0 +1,10 @@
+//double i = 32/5;
+//		sop(i);
+package TypeCasting;
+
+public class TypeCasting13 {
+    public static void main(String[] args) {
+        double i=32/5;
+        System.out.println("value of double="+i);
+    }
+}
