3550aa4b8303a5dbff91ac1786100d017b97f111
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-26 13:26:17 +0530

    TypeCasting program with question

---
+++ core java/src/TypeCasting/TypeCasting11.java
@@ -0,0 +1,10 @@
+//char a = 65;
+//		sop(a);
+package TypeCasting;
+
+public class TypeCasting11 {
+    public static void main(String[] args) {
+        char a=65;
+        System.out.println("value of char="+a);
+    }
+}
