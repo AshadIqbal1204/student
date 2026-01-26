e7d74294245342d6e02204fe71c566894231d529
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-26 13:25:44 +0530

    TypeCasting program with question

---
+++ core java/src/TypeCasting/TypeCasting08.java
@@ -0,0 +1,11 @@
+//	8.	wap to define a byte variable and assign its value to an int type variable and print both variables.
+package TypeCasting;
+
+public class TypeCasting08 {
+    public static void main(String[] args) {
+        byte b=23;
+        int i=(int)b;
+        System.out.println("byte value="+b);
+        System.out.println("int value="+i);
+    }
+}
