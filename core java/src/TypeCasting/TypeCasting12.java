afbc970b09205683d9928734d92a56a6d76b9051
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-26 13:26:28 +0530

    TypeCasting program with question

---
+++ core java/src/TypeCasting/TypeCasting12.java
@@ -0,0 +1,10 @@
+//int s = 'a';
+//		sop(s);
+package TypeCasting;
+
+public class TypeCasting12 {
+    public static void main(String[] args) {
+        int s='a';
+        System.out.println("value of int="+s);
+    }
+}
