d7df72167987b55724096c180a761ec793d1310e
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-26 13:26:05 +0530

    TypeCasting program with question

---
+++ core java/src/TypeCasting/TypeCasting10.java
@@ -0,0 +1,10 @@
+//	int i = 19/5;
+//		sop(i);
+package TypeCasting;
+
+public class TypeCasting10 {
+    public static void main(String[] args) {
+        int i=19/5;
+        System.out.println("value of int="+i);
+    }
+}
