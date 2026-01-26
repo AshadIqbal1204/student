9e73a7237293880ee09d6a5674893ab6e5d39c7f
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-26 13:25:55 +0530

    TypeCasting program with question

---
+++ core java/src/TypeCasting/TypeCasting09.java
@@ -0,0 +1,11 @@
+//	9.	wap to define a double and assing it to a float variable and print both variables
+package TypeCasting;
+
+public class TypeCasting09 {
+    public static void main(String[] args) {
+        double d=56.6;
+        float f=(float)d;
+        System.out.println("value of double="+d);
+        System.out.println("value of float="+f);
+    }
+}
