8900bb646f96944811d84094e62a6d10d97b958c
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-26 13:27:03 +0530

    TypeCasting program with question

---
+++ core java/src/TypeCasting/TypeCasting15.java
@@ -0,0 +1,10 @@
+//int sum = 'A' + ' ';
+//		sop(sum);
+package TypeCasting;
+
+public class TypeCasting15 {
+    public static void main(String[] args) {
+        int sum='A'+' ';
+        System.out.println("value of int="+sum);
+    }
+}
