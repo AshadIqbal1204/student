9be7366846876a369daac8f44d39886162f4ffae
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-26 13:26:51 +0530

    TypeCasting program with question

---
+++ core java/src/TypeCasting/TypeCasting14.java
@@ -0,0 +1,10 @@
+//float i = 47/5;
+//		sop(i);
+package TypeCasting;
+
+public class TypeCasting14 {
+    public static void main(String[] args) {
+        float i=47/5;
+        System.out.println("value of float="+i);
+    }
+}
