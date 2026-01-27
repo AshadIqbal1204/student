c5214dfb703d9c0bb85f1ac8c6241b823a6adb98
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-27 12:30:13 +0530

    Arithmetic Operators program with question

---
+++ core java/src/ArithmeticOperator/AverageOfFourNumbers.java
@@ -0,0 +1,14 @@
+//	10.	wap to find average of 4 numbers?
+package ArithmeticOperator;
+
+public class AverageOfFourNumbers {
+    public static void main(String[] args) {
+        int a=45;
+        int b=35;
+        int c=13;
+        int d=38;
+        float e=(a+b+c+d)/4f;
+        System.out.println(e);
+
+    }
+}
