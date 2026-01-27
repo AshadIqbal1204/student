acbacbb93d1caec48e698d4fc08d5bb6fa4fccf6
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-27 12:30:26 +0530

    Arithmetic Operators program with question

---
+++ core java/src/ArithmeticOperator/AverageOfThreeNumber.java
@@ -0,0 +1,13 @@
+//	9.	wap to find average of 3 numbers?
+package ArithmeticOperator;
+
+public class AverageOfThreeNumber {
+    public static void main(String[] args) {
+        int a=56;
+        int b=23;
+        int c=36;
+        float d=(a+b+c)/3f;
+        System.out.println(d);
+
+    }
+}
