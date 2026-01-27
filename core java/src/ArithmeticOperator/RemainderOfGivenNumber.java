a29ca4e5dc2ff7982cf0cd5e65426d4fecdb0a78
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-27 12:31:09 +0530

    Arithmetic Operators program with question

---
+++ core java/src/ArithmeticOperator/RemainderOfGivenNumber.java
@@ -0,0 +1,12 @@
+//	7.	wap to find remainder of a given numbers?
+package ArithmeticOperator;
+
+public class RemainderOfGivenNumber {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=45;
+        b=5;
+        c=a/b;
+        System.out.println(c);
+    }
+}
