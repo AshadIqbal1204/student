85f7855a46e8c345e44146702c996f6136a55a93
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-27 12:29:59 +0530

    Arithmetic Operators program with question

---
+++ core java/src/AddThreeNum.java
@@ -0,0 +1,9 @@
+public class AddThreeNum {
+    public static void main(String[] args) {
+        int num1=34;
+        int num2=25;
+        int num3=45;
+        int num4=num1+num2+num3;
+        System.out.println("sum of three number="+num4);
+    }
+}
