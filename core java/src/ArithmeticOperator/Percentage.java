0acc524a6b2b63b219eaff491df539f940e73537
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-27 12:30:55 +0530

    Arithmetic Operators program with question

---
+++ core java/src/ArithmeticOperator/Percentage.java
@@ -0,0 +1,11 @@
+//	8.	wap to find percentage?
+package ArithmeticOperator;
+
+public class Percentage {
+    public static void main(String[] args) {
+        float a=500f;
+        float b=110f;
+        float c=(a/b)*100f;
+        System.out.println("percentage "+c+"%");
+    }
+}
