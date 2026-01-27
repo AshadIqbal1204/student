3fa4b736498e287fea5b4414009d27719865ce0e
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-27 12:30:44 +0530

    Arithmetic Operators program with question

---
+++ core java/src/ArithmeticOperator/MultiplyThreeNumber.java
@@ -0,0 +1,13 @@
+//	6.	wap to multiply three numbers?
+package ArithmeticOperator;
+
+public class MultiplyThreeNumber {
+    public static void main(String[] args) {
+        int a,b,c,d;
+        a=45;
+        b=56;
+        c=23;
+        d=a*b*c;
+        System.out.println(d);
+    }
+}
