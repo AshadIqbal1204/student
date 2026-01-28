9a34b518848c0eb5e53f75a7ba22aacf4f84f3e9
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-28 13:12:31 +0530

    Arithmetic Operators program with question

---
+++ core java/src/ArithmeticOperator/ProblemSums05.java
@@ -0,0 +1,15 @@
+//16.	int x = 42;
+//        	double y = 42.25;
+//		System.out.println("x mod 10 = " + x % 10 );
+//        	System.out.println("y mod 10 = " + y % 10 );
+//
+package ArithmeticOperator;
+
+public class ProblemSums05 {
+    public static void main(String[] args) {
+        int x=42;
+        double y=42.25;
+        System.out.println("x mod 10="+x%10);
+        System.out.println("y mod 10="+y%10);
+    }
+}
