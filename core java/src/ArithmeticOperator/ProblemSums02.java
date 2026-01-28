9e3959b05aa5e5c7c2160ae8e6217438b580d2f7
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-28 13:11:53 +0530

    Arithmetic Operators program with question

---
+++ core java/src/ArithmeticOperator/ProblemSums02.java
@@ -0,0 +1,9 @@
+//	13.	what will be output: double x = 14 / 5 ;
+package ArithmeticOperator;
+
+public class ProblemSums02 {
+    public static void main(String[] args) {
+        double x=14/5;
+        System.out.println(x);
+    }
+}
