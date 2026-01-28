b226a8aba358e83b4e8fb948dfe0a7d8b71cc120
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-28 13:11:42 +0530

    Arithmetic Operators program with question

---
+++ core java/src/ArithmeticOperator/ProblemSums.java
@@ -0,0 +1,9 @@
+//	12.	what will be output: int x = 5 + 3 * 4 / 5 + 4;
+package ArithmeticOperator;
+
+public class ProblemSums {
+    public static void main(String[] args) {
+        int x=5+3*4/5+4;
+        System.out.println(x);
+    }
+}
