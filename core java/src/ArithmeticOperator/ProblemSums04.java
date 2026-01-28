42a9a20698b40190ce14ba20077e1845058d3779
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-28 13:12:16 +0530

    Arithmetic Operators program with question

---
+++ core java/src/ArithmeticOperator/ProblemSums04.java
@@ -0,0 +1,15 @@
+//15.	int i = 34.0;
+//		int j = 7;
+//		int k = i % j;
+//		System.out.println("k = " + k );
+//
+package ArithmeticOperator;
+
+public class ProblemSums04 {
+    public static void main(String[] args) {
+        int i=34;
+        int j=7;
+        int k=i%j;
+        System.out.println("k="+k);
+    }
+}
