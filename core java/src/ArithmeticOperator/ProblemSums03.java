cffa7b6ba01620fa9bc99b1f6a67b63a13412b8f
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-28 13:12:06 +0530

    Arithmetic Operators program with question

---
+++ core java/src/ArithmeticOperator/ProblemSums03.java
@@ -0,0 +1,15 @@
+//	14.	int x = 15;
+//		int y = x % 4;
+//		int z = y * 24;
+//		System.out.println(z / 2);
+//
+package ArithmeticOperator;
+
+public class ProblemSums03 {
+    public static void main(String[] args) {
+        int x=15;
+        int y=x%4;
+        int z=y*24;
+        System.out.println(z/2);
+    }
+}
