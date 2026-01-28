a554e0b3457374af6ad5307495a543480a2bbead
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-28 13:11:24 +0530

    Arithmetic Operators program with question

---
+++ core java/src/ArithmeticOperator/AverageOfTwoNumber.java
@@ -0,0 +1,12 @@
+//	11.	wap to find average of 2 numbers?
+package ArithmeticOperator;
+
+public class AverageOfTwoNumber {
+    public static void main(String[] args) {
+        int a=45;
+        int b=34;
+        float c=(a+b)/2f;
+        System.out.println("average="+c);
+
+    }
+}
