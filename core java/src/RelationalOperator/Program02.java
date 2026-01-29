c1dc42a92bffb0d4284f3f905d124b7dc73094d1
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-29 13:12:10 +0530

    Relational Operators program with question

---
+++ core java/src/RelationalOperator/Program02.java
@@ -0,0 +1,19 @@
+//4.	int a = 10;
+//		int b = 15;
+//		a++;
+//		b--;
+//		int c = b % a;
+//		System.out.println(c >= b);
+//
+package RelationalOperator;
+
+public class Program02 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=15;
+        a++;
+        b--;
+        int c=b%a;
+        System.out.println(c>=b);
+    }
+}
