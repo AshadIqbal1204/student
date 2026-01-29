bcfa78054b3a4e448b14afb004b91d401a46ebf5
amaniqbal613 <amaniqbal466@gmail.com>
2026-01-29 13:11:52 +0530

    Relational Operators program with question

---
+++ core java/src/RelationalOperator/Program01.java
@@ -0,0 +1,16 @@
+//int a = 10;
+//		int b = a - 5;
+//		System.out.println(a <= b);
+//		System.out.println(a > b);
+//
+package RelationalOperator;
+
+public class Program01 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=a-5;
+        System.out.println(a<=b);
+        System.out.println(a>b);
+
+    }
+}
